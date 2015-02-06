d9f7f6451ddb12f8959ff8f371efb88af79ffeb1
copuzzle@gmail.com <copuzzle@gmail.com>
2015-02-06 14:32:29 +0800

    添加第三版本支付(jsapi)

---
+++ wechat.class.php
@@ -0,0 +1,508 @@
+<?php
+
+class Wechat
+{
+    private $appid;
+    private $appsecret;
+    private $partnerid;
+    private $partnerkey;
+    private $paysignkey;
+    public $debug = false;
+
+    public function __construct($options)
+    {
+        $this->appid = isset($options['appid']) ? $options['appid'] : '';
+        $this->appsecret = isset($options['appsecret']) ? $options['appsecret'] : '';
+        $this->partnerid = isset($options['partnerid']) ? $options['partnerid'] : '';
+        $this->partnerkey = isset($options['partnerkey']) ? $options['partnerkey'] : '';
+        $this->debug = isset($options['debug']) ? $options['debug'] : false;
+    }
+
+
+    public static function xmlSafeStr($str)
+    {
+        return '<![CDATA['.preg_replace("/[\\x00-\\x08\\x0b-\\x0c\\x0e-\\x1f]/",'',$str).']]>';
+    }
+    /**
+     * 数据XML编码
+     * @param mixed $data 数据
+     * @return string
+     */
+    public static function data_to_xml($data)
+    {
+        $xml = '';
+        foreach ($data as $key => $val) {
+            is_numeric($key) && $key = "item id=\"$key\"";
+            $xml .= "<$key>";
+            $xml .= (is_array($val) || is_object($val)) ? self::data_to_xml($val) : self::xmlSafeStr($val);
+            list($key,) = explode(' ', $key);
+            $xml .= "</$key>";
+        }
+        return $xml;
+    }
+
+    /**
+     * XML编码
+     * @param mixed $data 数据
+     * @param string $root 根节点名
+     * @param string $item 数字索引的子节点名
+     * @param string $attr 根节点属性
+     * @param string $id 数字索引子节点key转换的属性名
+     * @param string $encoding 数据编码
+     * @return string
+     */
+    public function xml_encode($data, $root = 'xml', $item = 'item', $attr = '', $id = 'id', $encoding = 'utf-8')
+    {
+        if (is_array($attr)) {
+            $_attr = array();
+            foreach ($attr as $key => $value) {
+                $_attr[] = "{$key}=\"{$value}\"";
+            }
+            $attr = implode(' ', $_attr);
+        }
+        $attr = trim($attr);
+        $attr = empty($attr) ? '' : " {$attr}";
+        $xml = "<{$root}{$attr}>";
+        $xml .= self::data_to_xml($data, $item, $id);
+        $xml .= "</{$root}>";
+        return $xml;
+    }
+
+
+    /**
+     * GET 请求
+     * @param string $url
+     * @return string
+     */
+    private function http_get($url)
+    {
+        $oCurl = curl_init();
+        if (stripos($url, "https://") !== FALSE) {
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, FALSE);
+            curl_setopt($oCurl, CURLOPT_SSLVERSION, 1); //CURL_SSLVERSION_TLSv1
+        }
+        curl_setopt($oCurl, CURLOPT_URL, $url);
+        curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
+        $sContent = curl_exec($oCurl);
+        $aStatus = curl_getinfo($oCurl);
+        curl_close($oCurl);
+        if (intval($aStatus["http_code"]) == 200) {
+            return $sContent;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * POST 请求
+     * @param string $url
+     * @param array $param
+     * @param boolean $post_file 是否文件上传
+     * @return string content
+     */
+    private function http_post($url, $param, $post_file = false)
+    {
+        $oCurl = curl_init();
+        if (stripos($url, "https://") !== FALSE) {
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+            curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($oCurl, CURLOPT_SSLVERSION, 1); //CURL_SSLVERSION_TLSv1
+        }
+        if (is_string($param) || $post_file) {
+            $strPOST = $param;
+        } else {
+            $aPOST = array();
+            foreach ($param as $key => $val) {
+                $aPOST[] = $key . "=" . urlencode($val);
+            }
+            $strPOST = join("&", $aPOST);
+        }
+        curl_setopt($oCurl, CURLOPT_URL, $url);
+        curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($oCurl, CURLOPT_POST, true);
+        curl_setopt($oCurl, CURLOPT_POSTFIELDS, $strPOST);
+        $sContent = curl_exec($oCurl);
+        $aStatus = curl_getinfo($oCurl);
+        curl_close($oCurl);
+        if (intval($aStatus["http_code"]) == 200) {
+            return $sContent;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 微信api不支持中文转义的json结构
+     * @param array $arr
+     * @return string
+     */
+    static function json_encode($arr)
+    {
+        $parts = array();
+        $is_list = false;
+        //Find out if the given array is a numerical array
+        $keys = array_keys($arr);
+        $max_length = count($arr) - 1;
+        if (($keys [0] === 0) && ($keys [$max_length] === $max_length)) { //See if the first key is 0 and last key is length - 1
+            $is_list = true;
+            for ($i = 0; $i < count($keys); $i++) { //See if each key correspondes to its position
+                if ($i != $keys [$i]) { //A key fails at position check.
+                    $is_list = false; //It is an associative array.
+                    break;
+                }
+            }
+        }
+        foreach ($arr as $key => $value) {
+            if (is_array($value)) { //Custom handling for arrays
+                if ($is_list)
+                    $parts [] = self::json_encode($value); /* :RECURSION: */
+                else
+                    $parts [] = '"' . $key . '":' . self::json_encode($value); /* :RECURSION: */
+            } else {
+                $str = '';
+                if (!$is_list)
+                    $str = '"' . $key . '":';
+                //Custom handling for multiple data types
+                if (!is_string($value) && is_numeric($value) && $value < 2000000000)
+                    $str .= $value; //Numbers
+                elseif ($value === false)
+                    $str .= 'false'; //The booleans
+                elseif ($value === true)
+                    $str .= 'true';
+                else
+                    $str .= '"' . addslashes($value) . '"'; //All other things
+                // :TODO: Is there any more datatype we should be in the lookout for? (Object?)
+                $parts [] = $str;
+            }
+        }
+        $json = implode(',', $parts);
+        if ($is_list)
+            return '[' . $json . ']'; //Return numerical JSON
+        return '{' . $json . '}'; //Return associative JSON
+    }
+
+    /**
+     * 生成随机字串
+     * @param number $length 长度，默认为16，最长为32字节
+     * @return string
+     */
+    static function generateNonceStr($length = 16)
+    {
+        // 密码字符集，可任意添加你需要的字符
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        $str = "";
+        for ($i = 0; $i < $length; $i++) {
+            $str .= $chars[mt_rand(0, strlen($chars) - 1)];
+        }
+        return $str;
+    }
+
+    /**
+     * 通用签名方法
+     * @param $data_arr 签名数据
+     * @param string $partner_key 签名密钥
+     * @param string $method 方法
+     * @return string
+     */
+    static function commonSign($data_arr, $partner_key = '', $method = 'md5')
+    {
+        if (!function_exists($method)) return false;
+        ksort($data_arr);
+        $param_str = "";
+        foreach ($data_arr as $key => $value) {
+            if (strlen($value) != 0) {
+                $param_str .= $key . "=" . $value . '&';
+            }
+        }
+        if ($partner_key) {
+            $param_str .= 'key=' . $partner_key;
+        } else {
+            $param_str = rtrim($param_str, '&');
+        }
+        return strtoupper($method($param_str));
+    }
+
+    /**
+     * 创建红包签名
+     * @param $params
+     * @return xml string
+     */
+    public function create_red_pack($arr_data)
+    {
+        $arr_data['mch_id'] = $this->partnerid;
+        $arr_data['wxappid'] = $this->appid;
+        $pay_sign_key = $this->paysignkey;
+        ksort($arr_data);
+        $param_str = "";
+        $arr_data['sign'] = self::commonSign($arr_data, $this->partnerkey, 'md5');
+        $xml_pack = $this->data_to_xml($arr_data);
+        return $xml_pack;
+
+    }
+
+    /**
+     * 统一下单接口
+     * @param $data_arr
+     * @return array
+     */
+    public function uniFidoOrder($data_arr)
+    {
+        $server_url = 'https://api.mch.weixin.qq.com/pay/unifiedorder';
+        $data_arr['appid'] = $this->appid;
+        $data_arr['mch_id'] = $this->partnerid;
+        $data_arr['nonce_str'] = self::generateNonceStr();
+        $sign = $this->commonSign($data_arr, $this->partnerkey, 'md5');
+        $data_arr['sign'] = $sign;
+        $xml_pack = '<xml>' . $this->data_to_xml($data_arr) . '</xml>';
+        $res = $this->http_post($server_url, $xml_pack);
+        $resp_data = (array)simplexml_load_string($res, 'SimpleXMLElement', LIBXML_NOCDATA);
+        return $resp_data;
+    }
+
+    public function createJSAPIData($prepay_id)
+    {
+        $package_str = 'prepay_id=' . $prepay_id;
+        $data_arr = array(
+            'appId' => $this->appid,
+            'timeStamp' => strval(intval(time())),
+            'nonceStr' => $this->generateNonceStr(),
+            'package' => $package_str,
+            'signType' => 'MD5',
+        );
+        $data_arr['paySign'] = $this->commonSign($data_arr, $this->partnerkey, 'md5');
+        return $data_arr;
+    }
+
+    /**
+     * 回调通知签名验证
+     * @param array $orderxml 返回的orderXml的数组表示，留空则自动从post数据获取
+     * @return boolean
+     */
+    public function checkOrderSign($orderxml = '')
+    {
+        if (!$orderxml) {
+            $postStr = file_get_contents("php://input");
+            if (!empty($postStr)) {
+                $data_arr = (array)simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            } else
+            {
+                return false;
+            };
+            $wx_sign = $data_arr['sign'];
+            unset($data_arr['sign']);
+            $local_sign = $this->commonSign($data_arr, $this->partnerkey, 'md5');
+            if ($wx_sign != $local_sign) return false;
+            return $data_arr;
+        }
+        return false;
+    }
+}
+
+/**
+ * PKCS7Encoder class
+ *
+ * 提供基于PKCS7算法的加解密接口.
+ */
+class PKCS7Encoder
+{
+    public static $block_size = 32;
+
+    /**
+     * 对需要加密的明文进行填充补位
+     * @param $text 需要进行填充补位操作的明文
+     * @return 补齐明文字符串
+     */
+    function encode($text)
+    {
+        $block_size = PKCS7Encoder::$block_size;
+        $text_length = strlen($text);
+        //计算需要填充的位数
+        $amount_to_pad = PKCS7Encoder::$block_size - ($text_length % PKCS7Encoder::$block_size);
+        if ($amount_to_pad == 0) {
+            $amount_to_pad = PKCS7Encoder::block_size;
+        }
+        //获得补位所用的字符
+        $pad_chr = chr($amount_to_pad);
+        $tmp = "";
+        for ($index = 0; $index < $amount_to_pad; $index++) {
+            $tmp .= $pad_chr;
+        }
+        return $text . $tmp;
+    }
+
+    /**
+     * 对解密后的明文进行补位删除
+     * @param decrypted 解密后的明文
+     * @return 删除填充补位后的明文
+     */
+    function decode($text)
+    {
+
+        $pad = ord(substr($text, -1));
+        if ($pad < 1 || $pad > PKCS7Encoder::$block_size) {
+            $pad = 0;
+        }
+        return substr($text, 0, (strlen($text) - $pad));
+    }
+
+}
+
+/**
+ * Prpcrypt class
+ *
+ * 提供接收和推送给公众平台消息的加解密接口.
+ */
+class Prpcrypt
+{
+    public $key;
+
+    function Prpcrypt($k)
+    {
+        $this->key = base64_decode($k . "=");
+    }
+
+    /**
+     * 对明文进行加密
+     * @param string $text 需要加密的明文
+     * @return string 加密后的密文
+     */
+    public function encrypt($text, $appid)
+    {
+
+        try {
+            //获得16位随机字符串，填充到明文之前
+            $random = $this->getRandomStr();//"aaaabbbbccccdddd";
+            $text = $random . pack("N", strlen($text)) . $text . $appid;
+            // 网络字节序
+            $size = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+            $module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
+            $iv = substr($this->key, 0, 16);
+            //使用自定义的填充方式对明文进行补位填充
+            $pkc_encoder = new PKCS7Encoder;
+            $text = $pkc_encoder->encode($text);
+            mcrypt_generic_init($module, $this->key, $iv);
+            //加密
+            $encrypted = mcrypt_generic($module, $text);
+            mcrypt_generic_deinit($module);
+            mcrypt_module_close($module);
+
+            //			print(base64_encode($encrypted));
+            //使用BASE64对加密后的字符串进行编码
+            return array(ErrorCode::$OK, base64_encode($encrypted));
+        } catch (Exception $e) {
+            //print $e;
+            return array(ErrorCode::$EncryptAESError, null);
+        }
+    }
+
+    /**
+     * 对密文进行解密
+     * @param string $encrypted 需要解密的密文
+     * @return string 解密得到的明文
+     */
+    public function decrypt($encrypted, $appid)
+    {
+
+        try {
+            //使用BASE64对需要解密的字符串进行解码
+            $ciphertext_dec = base64_decode($encrypted);
+            $module = mcrypt_module_open(MCRYPT_RIJNDAEL_128, '', MCRYPT_MODE_CBC, '');
+            $iv = substr($this->key, 0, 16);
+            mcrypt_generic_init($module, $this->key, $iv);
+            //解密
+            $decrypted = mdecrypt_generic($module, $ciphertext_dec);
+            mcrypt_generic_deinit($module);
+            mcrypt_module_close($module);
+        } catch (Exception $e) {
+            return array(ErrorCode::$DecryptAESError, null);
+        }
+
+
+        try {
+            //去除补位字符
+            $pkc_encoder = new PKCS7Encoder;
+            $result = $pkc_encoder->decode($decrypted);
+            //去除16位随机字符串,网络字节序和AppId
+            if (strlen($result) < 16)
+                return "";
+            $content = substr($result, 16, strlen($result));
+            $len_list = unpack("N", substr($content, 0, 4));
+            $xml_len = $len_list[1];
+            $xml_content = substr($content, 4, $xml_len);
+            $from_appid = substr($content, $xml_len + 4);
+            if (!$appid)
+                $appid = $from_appid;
+            //如果传入的appid是空的，则认为是订阅号，使用数据中提取出来的appid
+        } catch (Exception $e) {
+            //print $e;
+            return array(ErrorCode::$IllegalBuffer, null);
+        }
+        if ($from_appid != $appid)
+            return array(ErrorCode::$ValidateAppidError, null);
+        //不注释上边两行，避免传入appid是错误的情况
+        return array(0, $xml_content, $from_appid); //增加appid，为了解决后面加密回复消息的时候没有appid的订阅号会无法回复
+
+    }
+
+
+    /**
+     * 随机生成16位字符串
+     * @return string 生成的字符串
+     */
+    function getRandomStr()
+    {
+
+        $str = "";
+        $str_pol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+        $max = strlen($str_pol) - 1;
+        for ($i = 0; $i < 16; $i++) {
+            $str .= $str_pol[mt_rand(0, $max)];
+        }
+        return $str;
+    }
+
+}
+
+/**
+ * error code
+ * 仅用作类内部使用，不用于官方API接口的errCode码
+ */
+class ErrorCode
+{
+    public static $OK = 0;
+    public static $ValidateSignatureError = 40001;
+    public static $ParseXmlError = 40002;
+    public static $ComputeSignatureError = 40003;
+    public static $IllegalAesKey = 40004;
+    public static $ValidateAppidError = 40005;
+    public static $EncryptAESError = 40006;
+    public static $DecryptAESError = 40007;
+    public static $IllegalBuffer = 40008;
+    public static $EncodeBase64Error = 40009;
+    public static $DecodeBase64Error = 40010;
+    public static $GenReturnXmlError = 40011;
+    public static $errCode = array(
+        '0' => '处理成功',
+        '40001' => '校验签名失败',
+        '40002' => '解析xml失败',
+        '40003' => '计算签名失败',
+        '40004' => '不合法的AESKey',
+        '40005' => '校验AppID失败',
+        '40006' => 'AES加密失败',
+        '40007' => 'AES解密失败',
+        '40008' => '公众平台发送的xml不合法',
+        '40009' => 'Base64编码失败',
+        '40010' => 'Base64解码失败',
+        '40011' => '公众帐号生成回包xml失败'
+    );
+
+    public static function getErrText($err)
+    {
+        if (isset(self::$errCode[$err])) {
+            return self::$errCode[$err];
+        } else {
+            return false;
+        };
+    }
+}
